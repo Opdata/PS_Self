a2097dabf84ba09661b73c758a44d0c6516729d4
Opdata <pb03641@gmail.com>
2021-02-12 19:37:22 +0900

    [Not] PY-Valid Parenthese

---
+++ leetcode/venv/Include/[Not]Valid Parenthese.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        mapping = {
+            ')' : '(',
+            '}' : '{',
+            ']' : '[',
+        }
+        
+        for char in s:
+            if char not in mapping:
+                stack.append(char)
+            elif not stack or mapping[char] != stack.pop():
+                return False
+            
+        return len(stack) == 0
